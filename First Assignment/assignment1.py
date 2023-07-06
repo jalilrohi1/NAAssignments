348389550b6bb45dfa5d7a1909c5f7493b892628
Luigi301 <ignazio.liotta98@gmail.com>
2023-07-06 17:04:07 +0200

    added new metrics

--- First Assignment/assignment1.py
+++ First Assignment/assignment1.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import logging
 import numpy as np
-import powerlaw
 from tabulate import tabulate
 
 
@@ -27,6 +26,12 @@ def table_results(metrics, values):
     table = zip(metrics, values)
     return tabulate(table, headers=headers)
 
+def column_results(header, values):
+    return tabulate(values, headers=header)
+
+def append_ten(lst_source, lst_dest):   #append first ten elements in source to dest
+    for x in range(10):
+        lst_dest.append(lst_source[x])
 
 def main(verbose: bool):
     if verbose:
@@ -34,9 +39,11 @@ def main(verbose: bool):
 
     metrics = ["Number of nodes", "Number of edges", "Lowerbound Diameter", "Minimum Degree", "Average degree", "Highest Degree", 
            "Average Centrality", "Top Centrality", "Transitivity", "Average Number of Triangles", "Highest Number of Triangles", 
-           "Density", "K-Core", "K-Core Minimum Degree"]
+           "Density", "K-Core", "K-Core Minimum Degree", "Assortativity"]
     values = []
-    top_nodes = [] #TODO
+    top_degree = []
+    top_centrality = []
+    top_triangles = []
 
     print("Opening dataset...")
     f = open("../inf-italy-osm/inf-italy-osm-cleaned.edges", "r")
@@ -44,12 +51,11 @@ def main(verbose: bool):
     f.close()
 
     print("Calculating metrics...")
-
     logging.info("Calculating nodes...")
     values.append(G.number_of_nodes())
     logging.info("Calculating edges...")
     values.append(G.number_of_edges())
-    #print("diameter: ", nx.diameter(G)) #too slow
+    #values.append(nx.diameter(G)) #too slow
     logging.info("Calculating lower bound diameter...")
     values.append(nx.approximation.diameter(G))
 
@@ -59,6 +65,8 @@ def main(verbose: bool):
     values.append(min(degree_sequence))
     avg_degree = sum(degree_sequence)/G.number_of_nodes()
     values.append(avg_degree)
+    dg = sorted(G.degree, key=lambda x: x[1], reverse=True)
+    append_ten(dg, top_degree)
 
     logging.info("Calculating centrality...")
     centrality = nx.degree_centrality(G)
@@ -66,6 +74,7 @@ def main(verbose: bool):
     sorted_c = sorted(centrality.items(), key=lambda x:x[1], reverse=True)
     values.append(centrality_avg)
     values.append(sorted_c[0][1])
+    append_ten(sorted_c, top_centrality)
 
     #print("betweenness: ", nx.betweenness_centrality(G)) #this is very slow to calculate
     #print("approximated betweenness: ", nx.betweenness_centrality(G, k=100)) #still too slow
@@ -80,6 +89,7 @@ def main(verbose: bool):
     high_t = sorted(tmp.items(), key=lambda x:x[1], reverse=True)
     values.append(avg_triangles)
     values.append(high_t[0][1])
+    append_ten(high_t, top_triangles)
 
     logging.info("Calculating density...")
     values.append(nx.density(G))
@@ -89,7 +99,17 @@ def main(verbose: bool):
     values.append(KC)
     values.append(min(sorted((d for n, d in KC.degree()), reverse=True)))
 
+    logging.info("Calculating assortativity...")
+    values.append(nx.degree_assortativity_coefficient(G))
+
     print(table_results(metrics, values))
+    
+    print("\nTop 10 nodes for each metric\n")
+    print(column_results(["Node", "Degree"], top_degree))
+    print("\n")
+    print(column_results(["Node", "Centrality"], top_centrality))
+    print("\n")
+    print(column_results(["Node", "Triangles"], top_triangles))
 
     plt.figure(1)
     ax1 = plt.subplot()
@@ -99,41 +119,14 @@ def main(verbose: bool):
     ax1.set_ylabel("# of Nodes")
     plt.show()
 
-    plt.figure(2)
-    plt.plot(degree_sequence, 'b-')
-    plt.show()
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-v", action='store_true')
     cli_args = parser.parse_args()
     main(cli_args.v)
-    #xmin = powerlaw.find_xmin(degree_sequence)
-    """
-    fit = powerlaw.Fit(degree_sequence)
-    fit.distribution_compare('power_law', 'lognormal')
-    fig4 = fit.plot_ccdf(linewidth=3, color='black')
-    fit.power_law.plot_ccdf(ax=fig4, color='r', linestyle='--') #powerlaw
-    fit.lognormal.plot_ccdf(ax=fig4, color='g', linestyle='--') #lognormal
-    fit.stretched_exponential.plot_ccdf(ax=fig4, color='b', linestyle='--') #stretched_exponential
-    plt.show()
-
-    """
 
 """
 TODO:
-- random or scale free network https://en.wikipedia.org/wiki/Scale-free_network
-- important nodes (we can use centrality, number of triangles, max degree (gotta get the corresponding node though))
-- assortative (how?)
-- look for more questions in the slides
-
-INFO OBTAINED:
-- it is connected (lowest degree is 1)
-- max degree is 9
-- most common degree is 2
-- avg centrality is 3.18e-07
-- node 1489103 has highest centrality with 1.36e-06
-- transitivity is 0.00269
-- avg number of triangles is 0.00332
-- node 1013347 has highest number of triangles with 5
+- random or scale free network https://en.wikipedia.org/wiki/Scale-free_network -> not random, sort of scale free?
+- smoother graph -> https://www.geeksforgeeks.org/how-to-plot-a-smooth-curve-in-matplotlib/
 """
