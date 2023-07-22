e52e4c049be978be41f1694c6533bf1fac6f0d64
Luigi301 <ignazio.liotta98@gmail.com>
2023-07-22 12:06:46 +0200

    updated first assignment, more plots

--- First Assignment/assignment1.py
+++ First Assignment/assignment1.py
@@ -3,58 +3,85 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import logging
 import numpy as np
-from scipy.interpolate import make_interp_spline
 from tabulate import tabulate
+import pandas as pd
+import os
 
-"""
-basic metrics:
-V edges
-V nodes
-X diameter (too long?)
-V degree
-V centrality https://networkx.org/documentation/stable/reference/algorithms/centrality.html
-X betweenness https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.betweenness_centrality.html
-X closeness https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.centrality.closeness_centrality.html
-- brokerage https://github.com/rafguns/gefura
-V transitivity https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.cluster.transitivity.html
-V triangles https://networkx.org/documentation/stable/reference/algorithms/generated/networkx.algorithms.cluster.triangles.html
-"""
+def open_dataset(dataset:str) -> nx.Graph:
+    """Loads dataset from path"""
+    print("Opening dataset...")
+    f = open(dataset, "r")
+    G = nx.read_edgelist(f, nodetype=int)
+    G.remove_edges_from(nx.selfloop_edges(G))
+    f.close()
+    return G
 
-def table_results(metrics, values):
+def table_results(metrics:list[str], values:list) -> str:
+    """Print data in tabulate format"""
     headers = ["Metric", "Value"]
     table = zip(metrics, values)
     return tabulate(table, headers=headers)
 
-def column_results(header, values):
-    return tabulate(values, headers=header)
-
-def append_ten(lst_source, lst_dest):   #append first ten elements in source to dest
+def append_ten(lst_source:list, lst_dest:list) -> None:
+    """Add ten values from lst_source to lst_dest"""
     for x in range(10):
         lst_dest.append(lst_source[x])
 
-def main(dataset: str, verbose: bool):
+def save_metrics(values:list, metrics:list[str], output_dir:str, name:str):
+    """Save calculated metrics on disk as a png"""
+    fig, ax = plt.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    table = ax.table(cellText=[values], colLabels=metrics, loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.auto_set_column_width(col=list(range(len(metrics))))
+    fig.tight_layout()
+    fig.savefig(output_dir+name, bbox_inches="tight", pad_inches=0.5)
+
+def save_metrics_alt(values:list, metrics:list[str], output_dir:str, name:str):
+    """Alterative function to save metrics using dataframes instead of lists"""
+    fig, ax = plt.subplots()
+    fig.patch.set_visible(False)
+    ax.axis('off')
+    ax.axis('tight')
+    table = ax.table(cellText=values, colLabels=metrics, loc='center')
+    table.auto_set_font_size(False)
+    table.set_fontsize(10)
+    table.auto_set_column_width(col=list(range(len(metrics))))
+    fig.savefig(output_dir+name, bbox_inches="tight", pad_inches=0.5)
+
+
+def main(dataset:str, output_dir:str, full:bool, verbose:bool):
     if verbose:
         logging.basicConfig(level=logging.INFO)
 
-    metrics = ["Number of nodes", "Number of edges", "Lowerbound Diameter", "Minimum Degree", "Average degree", "Highest Degree", 
-           "Average Centrality", "Top Centrality", "Transitivity", "Average Number of Triangles", "Highest Number of Triangles", 
-           "Density", "K-Core", "K-Core Minimum Degree", "Assortativity"]
+    if full:
+        metrics = ["Number of nodes", "Number of edges", "Diameter", "Minimum Degree", "Average degree", "Highest Degree", 
+            "Average Centrality", "Top Centrality", "Transitivity", "Average Number of Triangles", "Highest Number of Triangles", 
+            "Density", "K-Core", "K-Core Minimum Degree", "Assortativity", "Average Betweenness", "Top Betweenness", 
+            "Average Closeness", "Top Closeness"]
+    else:
+        metrics = ["Number of nodes", "Number of edges", "Lowerbound Diameter", "Minimum Degree", "Average degree", "Highest Degree", 
+            "Average Centrality", "Top Centrality", "Transitivity", "Average Number of Triangles", "Highest Number of Triangles", 
+            "Density", "K-Core", "K-Core Minimum Degree", "Assortativity"]
+        
     values = []
     top_degree = []
     top_centrality = []
     top_triangles = []
+    if full:
+        top_betweenness = []
+        top_closeness = []
 
-    print("Opening dataset...")
-    f = open(dataset, "r")
-    G = nx.read_edgelist(f, nodetype=int)
-    f.close()
+    G = open_dataset(dataset)
 
     print("Calculating metrics...")
     logging.info("Calculating nodes...")
     values.append(G.number_of_nodes())
     logging.info("Calculating edges...")
     values.append(G.number_of_edges())
-    #values.append(nx.diameter(G)) #too slow
     logging.info("Calculating lower bound diameter...")
     values.append(nx.approximation.diameter(G))
 
@@ -74,10 +101,6 @@ def main(dataset: str, verbose: bool):
     values.append(centrality_avg)
     values.append(sorted_c[0][1])
     append_ten(sorted_c, top_centrality)
-
-    #print("betweenness: ", nx.betweenness_centrality(G)) #this is very slow to calculate
-    #print("approximated betweenness: ", nx.betweenness_centrality(G, k=100)) #still too slow
-    #print("closeness: ", nx.closeness_centrality(G)) #too slow
 
     logging.info("Calculating transitivity...")
     values.append(nx.transitivity(G))
@@ -101,44 +124,79 @@ def main(dataset: str, verbose: bool):
     logging.info("Calculating assortativity...")
     values.append(nx.degree_assortativity_coefficient(G))
 
-    print(table_results(metrics, values))
-    
-    print("\nTop 10 nodes for each metric\n")
-    print(column_results(["Node", "Degree"], top_degree))
-    print("\n")
-    print(column_results(["Node", "Centrality"], top_centrality))
-    print("\n")
-    print(column_results(["Node", "Triangles"], top_triangles))
+    if full:
+        logging.info("Calculating betweenness...")
+        betweenness = nx.betweenness_centrality(G)
+        betweenness_avg = sum(betweenness.values())/G.number_of_nodes()
+        sorted_b = sorted(betweenness.items(), key=lambda x:x[1], reverse=True)
+        values.append(betweenness_avg)
+        values.append(sorted_b[0][1])
+        append_ten(sorted_b, top_betweenness)
+        
+        logging.info("Calculating closeness...")
+        closeness = nx.closeness_centrality(G)
+        closeness_avg = sum(closeness.values())/G.number_of_nodes()
+        sorted_clo = sorted(closeness.items(), key=lambda x:x[1], reverse=True)
+        values.append(closeness_avg)
+        values.append(sorted_clo[0][1])
+        append_ten(sorted_clo, top_closeness)
 
-    plt.figure(1)
+    if verbose: print("\n", table_results(metrics, values))
+
+    if not os.path.exists(output_dir): os.mkdir(output_dir)
+
+    #degree histogram
+    fig = plt.figure()
     ax1 = plt.subplot()
     ax1.bar(*np.unique(degree_sequence, return_counts=True))
     ax1.set_title("Degree histogram")
     ax1.set_xlabel("Degree")
     ax1.set_ylabel("# of Nodes")
-    plt.show()
+    fig.savefig(output_dir+"degree-histogram.png")
 
-    #TODO: add more graphs about centrality and other metrics
-    
+    #degree distribution
+    fig = plt.figure()
     degree_counts = np.bincount(degree_sequence)
     degrees = np.arange(len(degree_counts))
     plt.plot(degrees, degree_counts, 'r-')
     plt.xlabel('Degree')
     plt.ylabel('Number of nodes')
     plt.title('Degree Distribution')
-    plt.grid(True)
-    plt.show()
+    fig.savefig(output_dir+"degree-distribution.png")
+
+    #metrics table
+    save_metrics(values[0:3], metrics[0:3], output_dir, "metrics-1.png")
+    save_metrics(values[3:6], metrics[3:6], output_dir, "metrics-2.png")
+    save_metrics(values[6:9], metrics[6:9], output_dir, "metrics-3.png")
+    save_metrics(values[9:12], metrics[9:12], output_dir, "metrics-4.png")
+    save_metrics(values[12:15], metrics[12:15], output_dir, "metrics-5.png")
+    if full:
+        save_metrics(values[15:], metrics[15:], output_dir, "metrics-6.png")
+
+    if full:
+        d = {'Nodes-Degree': [x[0] for x in top_degree], 'Degree': [y[1] for y in top_degree], 
+            'Nodes-Centrality': [x[0] for x in top_centrality], 'Centrality': [y[1] for y in top_centrality],
+            'Nodes-Triangles': [x[0] for x in top_triangles], 'Triangles': [y[1] for y in top_triangles],
+            'Nodes-Betweenness': [x[0] for x in top_betweenness], 'Betweenness': [y[1] for y in top_betweenness],
+            'Nodes-Closeness': [x[0] for x in top_closeness], 'Closeness': [y[1] for y in top_closeness]}
+        df = pd.DataFrame(data=d)
+
+    else:
+        d = {'Nodes-Degree': [x[0] for x in top_degree], 'Degree': [y[1] for y in top_degree], 
+            'Nodes-Centrality': [x[0] for x in top_centrality], 'Centrality': [y[1] for y in top_centrality],
+            'Nodes-Triangles': [x[0] for x in top_triangles], 'Triangles': [y[1] for y in top_triangles]}
+        df = pd.DataFrame(data=d)
+
+    #top 10 nodes for specific metrics    
+    if verbose: print("\n", df)
+    save_metrics_alt(df.values, df.columns, output_dir, "top10-nodes.png")
+        
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-f", type=str, help="dataset file", default="../inf-italy-osm/inf-italy-osm-cleaned.edges")
-    #TODO: add plottable flag like in assignment2.py
-    parser.add_argument("-v", action='store_true')
+    parser.add_argument("-o", type=str, help="ouput directory", default="outputs/")
+    parser.add_argument("-full", help="calculate slow metrics", action='store_true')
+    parser.add_argument("-v", help="verbose output", action='store_true')
     cli_args = parser.parse_args()
-    main(cli_args.f, cli_args.v)
-
-"""
-TODO:
-- random or scale free network https://en.wikipedia.org/wiki/Scale-free_network -> not random, sort of scale free?
-- smoother graph -> https://www.geeksforgeeks.org/how-to-plot-a-smooth-curve-in-matplotlib/
-"""
+    main(cli_args.f, cli_args.o, cli_args.full, cli_args.v)
--- Third Assignment/assignment3.py
+++ Third Assignment/assignment3.py
@@ -1,11 +1,30 @@
+from matplotlib.figure import Figure
+from matplotlib.legend import Legend
+from matplotlib.text import Text
 import networkx as nx
 import matplotlib.pyplot as plt
 import random
 import argparse
 import numpy as np
 import logging
+from dataclasses import dataclass, field
+import os
 
-def open_dataset(dataset):
+def generate_graphs(num_nodes:int, density:float, probability:float, seed:int) -> list[nx.Graph]:
+    """Returns a list of graphs using Networkx generators.
+        Graphs are: Barabasi-Albert, Barbell, Circular Ladder and Newman-Watts-Strogatz"""
+    logging.info("Generating Barabasi-Albert graph...")
+    barabasi = nx.barabasi_albert_graph(num_nodes, density, seed)
+    logging.info("Generating Barbell graph...")
+    barbell = nx.barbell_graph(int(num_nodes/2), int(num_nodes/2))
+    logging.info("Generating Circular Ladder graph...")
+    circular_ladder = nx.circular_ladder_graph(num_nodes)
+    logging.info("Generating Newman-Watts-Strogatz graph...")
+    nws = nx.newman_watts_strogatz_graph(num_nodes, density, probability, seed)
+    return barabasi, barbell, circular_ladder, nws
+
+def open_dataset(dataset:str) -> nx.Graph:
+    """Loads dataset from path"""
     print("Opening dataset...")
     f = open(dataset, "r")
     G = nx.read_edgelist(f, nodetype=int)
@@ -13,17 +32,223 @@ def open_dataset(dataset):
     f.close()
     return G
 
-def main():
-    logging.basicConfig(level=logging.INFO)
+def get_degree_distribution(G:nx.Graph) -> list[int]:
+    """Returns the degree distribution of a graph in list format"""
+    return sorted((d for n, d in G.degree()), reverse=True)
+
+def plot_degree_distribution(G:nx.Graph, graph_name:str, output_dir: str) -> None:
+    """Save in a directory the plot of the degree distribution of the graph"""
+    degree_counts = np.bincount(get_degree_distribution(G))
+    degrees = np.arange(len(degree_counts))
+    title = graph_name + " - Degree Distribution"
+    plt.plot(degrees, degree_counts, 'r-')
+    plt.xlabel('Degree')
+    plt.ylabel('Number of nodes')
+    plt.title(title)
+    plt.savefig(output_dir)
+    plt.close()
+
+def attack(G:nx.Graph, r_nodes: list) -> None:
+    """Remove nodes in the r_nodes list from the graph G"""
+    for node in r_nodes:
+        G.remove_node(node)
+
+def select_random_nodes(G:nx.Graph, chunk:int) -> list[int]:
+    """Get list of random nodes"""
+    node_list = list(G.nodes())
+    targets = []
+    if len(node_list) > chunk: targets = random.sample(node_list, k=chunk)
+    return targets
+
+def select_highest_degree_nodes(G:nx.Graph, chunk:int) -> list[int]:
+    """Select nodes with the highest degree"""
+    dg = sorted(G.degree, key=lambda x: x[1], reverse=True)
+    targets = []
+    if len(list(G.nodes())) > chunk:
+        for x in range(chunk):
+            targets.append(dg[x][0])
+    return targets
+
+def select_highest_betweenness_nodes(G:nx.Graph, chunk:int) -> list[int]:
+    """Select nodes with the highest betweenness"""
+    bg = sorted(nx.betweenness_centrality(G).items(), key=lambda x:x[1], reverse=True)
+    targets = []
+    if len(list(G.nodes())) > chunk:
+        for x in range(chunk):
+            targets.append(bg[x][0])
+    return targets
+
+def select_highest_closeness_nodes(G:nx.Graph, chunk:int) -> list[int]:
+    """Select nodes with the highest closeness"""
+    cg = sorted(nx.closeness_centrality(G).items(), key=lambda x:x[1], reverse=True)
+    targets = []
+    if len(list(G.nodes())) > chunk:
+        for x in range(chunk):
+            targets.append(cg[x][0])
+    return targets
+
+def select_highest_pagerank_nodes(G:nx.Graph, chunk:int) -> list[int]:
+    """Select nodes with the highest pagerank"""
+    pg = sorted(nx.pagerank(G).items(), key=lambda x:x[1], reverse=True)
+    targets = []
+    if len(list(G.nodes())) > chunk:
+        for x in range(chunk):
+            targets.append(pg[x][0])
+    return targets
+
+@dataclass
+class GraphMetrics:
+    """Class for keeping track of the changes in the graph as the attacks follow"""
+    size_of_GC: list = field(default_factory=list)
+    edges: list = field(default_factory=list)
+    diameter: list = field(default_factory=list)
+    avg_degree: list = field(default_factory=list)
+    avg_centrality: list = field(default_factory=list)
+
+    def add_new_cycle(self, soGC, edges, diam, dd, avcen):
+        """Save the metrics of the current attack cycle"""
+        self.size_of_GC.append(soGC)
+        self.edges.append(edges)
+        self.diameter.append(diam)
+        self.avg_degree.append(dd)
+        self.avg_centrality.append(avcen)
+
+def get_giant_component(G:nx.Graph) -> nx.Graph:
+    cc = max(nx.connected_components(G), key=len)
+    subG = G.subgraph(cc)
+    return subG
+
+def get_avg_degree(G:nx.Graph) -> float:
+    return sum([d for n, d in G.degree()])/G.number_of_nodes()
+
+def get_avg_centrality(G:nx.Graph) -> float:
+    return sum(nx.degree_centrality(G).values())/G.number_of_nodes()
+
+def get_avg_betweenness(G:nx.Graph) -> float:
+    return sum(nx.betweenness_centrality(G).values())/G.number_of_nodes()
+
+def get_avg_closeness(G:nx.Graph) -> float:
+    return sum(nx.closeness_centrality(G).values())/G.number_of_nodes()
+
+def setup_attacks(G:nx.Graph, percentage:float) -> tuple[int, int, list[nx.Graph], list[GraphMetrics]]:
+    """Setup the resources for the attacks"""
+    chunk = int(percentage*len(G.nodes()))
+    num_steps = int(G.number_of_nodes()/chunk)-1
+    graph_copies = [G.copy(), G.copy(), G.copy(), G.copy(), G.copy()]
+    graph_metrics = [GraphMetrics(), GraphMetrics(), GraphMetrics(), GraphMetrics(), GraphMetrics()]
+    return chunk, num_steps, graph_copies, graph_metrics
+
+def create_fig(intervals:list[int], y_random, y_degree, y_bet, y_close, y_page, y_label:str, title:str) -> tuple[Figure, Legend, Text]:
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    ax.plot(intervals, y_random, 'r-', label='Random')
+    ax.plot(intervals, y_degree, 'b-', label='Degree')
+    ax.plot(intervals, y_bet, 'g-', label='Betweenness')
+    ax.plot(intervals, y_close, 'c-', label='Closeness')
+    ax.plot(intervals, y_page, 'y-', label='Pagerank')
+    handles, labels = ax.get_legend_handles_labels()
+    lgd = ax.legend(handles, labels, loc='upper center', bbox_to_anchor=(0.5,-0.1))
+    text = ax.text(-0.2,1.05, "", transform=ax.transAxes)
+    ax.set_xlabel("Nodes removed")
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    return fig, lgd, text
+
+def save_metrics(intervals:list[int], list_GM:list[GraphMetrics], output_dir:str) -> None:
+    """Save measured metrics on the graph"""
+    fig1, lgd1, text1 = create_fig(intervals, list_GM[0].size_of_GC, list_GM[1].size_of_GC, list_GM[2].size_of_GC, 
+                                   list_GM[3].size_of_GC, list_GM[4].size_of_GC, "Size of Giant Component", 
+                                   "Evolution of Giant Component")
     
-    dataset = "../tech-pgp/tech-pgp.edges"
+    fig2, lgd2, text2 = create_fig(intervals, list_GM[0].edges, list_GM[1].edges, list_GM[2].edges, 
+                                   list_GM[3].edges, list_GM[4].edges, "Number of edges", 
+                                   "Number of edges across the attacks")
+    
+    fig3, lgd3, text3 = create_fig(intervals, list_GM[0].diameter, list_GM[1].diameter, list_GM[2].diameter, 
+                                   list_GM[3].diameter, list_GM[4].diameter, "Diameter", 
+                                   "Evolution of Diameter")
+    
+    fig4, lgd4, text4 = create_fig(intervals, list_GM[0].avg_degree, list_GM[1].avg_degree, list_GM[2].avg_degree, 
+                                   list_GM[3].avg_degree, list_GM[4].avg_degree, "Average Degree", 
+                                   "Average Degree across the attacks")
+    
+    fig5, lgd5, text5 = create_fig(intervals, list_GM[0].avg_centrality, list_GM[1].avg_centrality, list_GM[2].avg_centrality, 
+                                   list_GM[3].avg_centrality, list_GM[4].avg_centrality, "Average Centrality", 
+                                   "Average Centrality across the attacks")
+    
+    if not os.path.exists(output_dir): os.mkdir(output_dir)
+    logging.info("Plots will be saved in: %s", output_dir)
+
+    fig1.savefig(output_dir+"/giant-component.png", bbox_extra_artists=(text1, lgd1), bbox_inches='tight')
+    plt.close(fig1)
+    fig2.savefig(output_dir+"/edges.png", bbox_extra_artists=(text2, lgd2), bbox_inches='tight')
+    plt.close(fig2)
+    fig3.savefig(output_dir+"/diameter.png", bbox_extra_artists=(text3, lgd3), bbox_inches='tight')
+    plt.close(fig3)
+    fig4.savefig(output_dir+"/avg-degree.png", bbox_extra_artists=(text4, lgd4), bbox_inches='tight')
+    plt.close(fig4)
+    fig5.savefig(output_dir+"/avg-centrality.png", bbox_extra_artists=(text5, lgd5), bbox_inches='tight')
+    plt.close(fig5)
+
+def launch_attacks(G:nx.Graph, percentage:float, output_dir:str, graph_name:str) ->None:
+    """Main attack function"""
+    chunk, num_steps, list_graphs, list_GM = setup_attacks(G, percentage)
+    intervals = []
+    targets = [None, None, None, None, None]
+    logging.info("Starting attacks on %s...", graph_name)
+    for x in range(num_steps):
+        #get list of target nodes for each type of attack
+        targets[0] = select_random_nodes(list_graphs[0], chunk)
+        targets[1] = select_highest_degree_nodes(list_graphs[1], chunk)
+        targets[2] = select_highest_betweenness_nodes(list_graphs[2], chunk)
+        targets[3] = select_highest_closeness_nodes(list_graphs[3], chunk)
+        targets[4] = select_highest_pagerank_nodes(list_graphs[4], chunk)
+        
+        #apply all attacks to the respective copy of the dataset
+        for k in range(5):
+            attack(list_graphs[k], targets[k])
+            gc = get_giant_component(list_graphs[k])
+            list_GM[k].add_new_cycle(len(gc), list_graphs[k].number_of_edges(), nx.diameter(gc), get_avg_degree(list_graphs[k]), get_avg_centrality(list_graphs[k]))
+
+        intervals.append(chunk*x)
+    save_metrics(intervals, list_GM, output_dir+graph_name)
+
+def main(dataset:str, num_nodes:int, density:int, probability:float, seed:int, percentage:float,
+         verbose:bool, output_dir:str) -> None:
+
+    if verbose:
+        logging.basicConfig(level=logging.INFO)
+
     G = open_dataset(dataset)
+    barabasi, barbell, circular_ladder, nws = generate_graphs(num_nodes, density, probability, seed)
+
+    if not os.path.exists(output_dir): os.mkdir(output_dir)
+    dest = output_dir + "degree_distribution"
+    if not os.path.exists(dest): os.mkdir(dest)
+
+    print("Calcuating degree distributions...")
+    logging.info("Outputs will be saved in %s", dest)
+    plot_degree_distribution(barabasi, "Barabasi-Albert", dest+"/barabasi-albert.png")
+    plot_degree_distribution(barbell, "Barbell", dest+"/barbell.png")
+    plot_degree_distribution(circular_ladder, "Circular Ladder", dest+"/circular-ladder.png")
+    plot_degree_distribution(nws, "Newman-Watts-Strogatz", dest+"/newman-watts-strogatz.png")
+    plot_degree_distribution(G, dataset, dest+"/loaded-graph.png")
+
+    launch_attacks(barabasi, percentage, output_dir, "barabasi-albert")
+    launch_attacks(barbell, percentage, output_dir, "barbell")
+    launch_attacks(circular_ladder, percentage, output_dir, "circular-ladder")
+    launch_attacks(nws, percentage, output_dir,"newman-watts-strogatz")
+    launch_attacks(G, percentage, output_dir, "loaded-graph")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-f", type=str, help="dataset file", default="../tech-pgp/tech-pgp.edges")
-    parser.add_argument("-plot", action='store_true')
-    parser.add_argument("-v", action='store_true')
+    parser.add_argument("-f", type=str, help="dataset file", default="../tech-routers-rf/tech-routers-rf.mtx")
+    parser.add_argument("-n", type=int, help="number of nodes in generated datasets", default=100)
+    parser.add_argument("-d", type=int, help="density of generated graphs", default=4)
+    parser.add_argument("-prob", type=float, help="probability of new edge in generated graphs", default=0.3)
+    parser.add_argument("-seed", type=int, help="seed", default=None)
+    parser.add_argument("-perc", type=float, help="percentage of total nodes to attack at each round", default=0.1)
+    parser.add_argument("-v", help="verbose output", action='store_true')
+    parser.add_argument("-o", type=str, help="output directory", default="outputs/")
     cli_args = parser.parse_args()
-    #main(cli_args.f, cli_args.p, cli_args.d, cli_args.q, cli_args.i, cli_args.plot, cli_args.v)
-    main()
+    main(cli_args.f, cli_args.n, cli_args.d, cli_args.prob, cli_args.seed, cli_args.perc, cli_args.v, cli_args.o)
